02432bb2d6a7be5120906874fc343a3786bf769f
a11y_spec <72933157+a11y-spec@users.noreply.github.com>
2022-01-06 20:59:28 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-int main(int argc, char** argv)
+int main()
 {
  
     FILE *read;
     FILE *write;
     fopen_s(&read, "dll.dll", "rb");
     fopen_s(&write,"dll.txt", "w");
-    fseek(read, 0, SEEK_END);
+    fseek_s(read, 0, SEEK_END);
     unsigned int size = ftell(read);
-    fseek(read, 0, SEEK_SET);
+    fseek_s(read, 0, SEEK_SET);
     unsigned char *mem = (unsigned char*)malloc(size);
     printf("Size: %dkb\n", size);
  
